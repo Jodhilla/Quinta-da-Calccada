f4d351c33608ab80260250369952f243ecc7b0a5
Jody Sergison <jsergison@NW-COM-LGD-2.local>
2016-05-04 15:25:11 -0700

    Updates 5-4

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -300,3 +300,24 @@ app.controller('AppCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
     $scope.slideIndex = index;
   };
 })
+
+
+
+
+app.directive('dynamicHeight', function() {
+    return {
+        require: ['^ionSlideBox'],
+        link: function(scope, elem, attrs, slider) {
+            scope.$watch(function() {
+                return slider[0].__slider.selected();
+            }, function(val) {
+                //getting the height of viewport
+                var newHeight = window.getComputedStyle(elem.parents('ion-content')[0], null).getPropertyValue("height");
+                if (parseInt(newHeight) > 0) {
+                    var ionScrollTag = elem.find('ion-scroll')[0];
+                    ionScrollTag.style.height = newHeight;
+                }
+            });
+        }
+    };
+});
