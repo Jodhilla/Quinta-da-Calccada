e9f9a4a4b4c6eee1fed576c60f30612bc435c5dd
Jody Sergison <jsergison@NW-COM-LGD-2.local>
2016-05-06 09:28:17 -0700

    upload 5-6

--- www/js/app.js
+++ www/js/app.js
@@ -20,6 +20,7 @@ var app = angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'
   });
 })
 
+
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
@@ -282,7 +283,7 @@ app.controller("MediaController", function($scope, $cordovaMedia, $ionicLoading)
 
 });
 
-app.controller('AppCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
+app.controller('AppCtrl', function($scope, $state, $ionicSlideBoxDelegate, $ionicModal) {
 
   // Called to navigate to the main app
   $scope.startApp = function() {
@@ -299,4 +300,34 @@ app.controller('AppCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
   $scope.slideChanged = function(index) {
     $scope.slideIndex = index;
   };
+
+  $ionicModal.fromTemplateUrl('my-modal.html', {
+     scope: $scope,
+     animation: 'slide-in-up',
+  }).then(function(modal) {
+     $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+     $scope.modal.show();
+  };
+
+  $scope.closeModal = function() {
+     $scope.modal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+     $scope.modal.remove();
+  });
+
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+     // Execute action
+  });
+
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+     // Execute action
+  });
 })
