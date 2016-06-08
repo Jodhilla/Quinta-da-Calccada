1b84f5bb5087d1f5de8dae9a9e883a3955506adb
jodhilla <jodhilla@gmail.com>
2016-06-08 06:31:21 -0700

    6/8

--- www/js/app.js
+++ www/js/app.js
@@ -248,25 +248,7 @@ app.controller("MediaController", function($scope, $cordovaMedia, $ionicLoading)
 
 });
 
-app.controller('AppCtrl', function($scope, $state, $ionicSlideBoxDelegate, $ionicModal) {
 
-  // Called to navigate to the main app
-  $scope.startApp = function() {
-    $state.go('main');
-  };
-  $scope.next = function() {
-    $ionicSlideBoxDelegate.next();
-  };
-  $scope.previous = function() {
-    $ionicSlideBoxDelegate.previous();
-  };
-
-  // Called each time the slide changes
-  $scope.slideChanged = function(index) {
-    $scope.slideIndex = index;
-  };
-
-})
 
 app.controller('ImageZoom', function($scope, $ionicModal) {
 
